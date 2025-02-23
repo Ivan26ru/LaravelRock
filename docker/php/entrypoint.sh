60cb1e32378365d3a1535c7e81cad55414899363
Ivan <it-1ivan@ya.ru>
2025-02-23 23:11:13 +0500

    WIP update path for chmod

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -3,8 +3,8 @@
 composer install
 
 # Нужен для laravel
-sudo chown -R www-data:www-data /var/www/storage
-sudo chown -R www-data:www-data /var/www/bootstrap/cache
+sudo chown -R www-data:www-data /var/www/backend/storage
+sudo chown -R www-data:www-data /var/www/backend/bootstrap/cache
 
 php artisan key:generate
 exec "${@-php-fpm}"
